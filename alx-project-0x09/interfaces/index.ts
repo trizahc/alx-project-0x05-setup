95af02a60dfff5444c99ac2c42a10acc8ce079fa
trizahc <trizahchepkirui07@gmail.comm>
2025-06-18 12:38:41 -0500

    Generated Image based on user prompt

--- alx-project-0x09/interfaces/index.ts
+++ alx-project-0x09/interfaces/index.ts
@@ -4,12 +4,17 @@ export interface ReactComponentProps {
   children: ReactNode
 }
 
+
 export interface GeneratedImageProps {
   imageUrl: string
   prompt: string
   width?: string
   height?: string
   action: (imagePath: string) => void
+}
+
+export type RequestProps = {
+  prompt: string;
 }
 
 export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">
